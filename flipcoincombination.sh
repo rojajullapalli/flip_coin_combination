0352a2972517632958b98b796e191d916fd51891
rojajullapalli <rojajules@gmail.com>
2021-07-24 18:51:28 +0530

    triplet combination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -2,40 +2,44 @@
 echo "flip Coin Combination"
 declare  -A  coin;
 
-coin[headhead]=0;
-coin[tailtail]=0;
-coin[headtail]=0;
+coin[headheadhead]=0;
+coin[tailtailtail]=0;
+coin[headheadtail]=0;
+coin[headtailtail]=0;
 count=0;
 
 addtoDictionary()
 {
   if [ $1 -eq 0 ]
   then
-      coin[headhead]=`expr ${coin[headhead]} + 1`;
+     coin[headheadhead]=`expr ${coin[headheadhead]} + 1`;
   elif [ $1 -eq 1 ]
   then
-      coin[tailtail]=`expr ${coin[tailtail]} + 1`;
+     coin[tailtailtail]=`expr ${coin[tailtailtail]} + 1`;
+  elif [ $1 -eq 2 ]
+  then
+     coin[headheadtail]=`expr ${coin[headheadtail]} + 1`;
   else
-     coin[headtail]=`expr ${coin[headtail]} + 1`;
+    coin[headtailtail]=`expr ${coin[headtailtail]} + 1`;
   fi
 }
 
 while [ $count -le 25 ]
 do
-  flip=$((RANDOM%3)); 
+  flip=$((RANDOM%4));
   addtoDictionary $flip;
   ((++count))
 done
 
-perheadhead=`expr ${coin[headhead]} \* 100`;
-perheadhead=`expr $perheadhead / 25`;
-pertailtail=`expr ${coin[tailtail]} \* 100`;
-pertailtail=`expr $pertailtail / 25`;
-perheadtail=`expr ${coin[headtail]} \* 100`;
-perheadtail=`expr $perheadtail / 25`;
-echo "Total headhead count ${coin[headhead]}"
-echo "Total tailtail count ${coin[tailtail]}"
-echo "Total headtail count ${coin[headtail]}"
-echo "percentage of headhead is $perheadhead"
-echo "percentage of tailtail is $pertailtail"
-echo "percentage of headtail is $perheadtail"
+perheadheadhead=`expr ${coin[headheadhead]} \* 100`;
+perheadheadhead=`expr $perheadheadhead / 25`;
+pertailtailtail=`expr ${coin[tailtailtail]} \* 100`;
+pertailtailtail=`expr $pertailtailtail / 25`;
+perheadheadtail=`expr ${coin[headheadtail]} \* 100`;
+perheadheadtail=`expr $perheadheadtail / 25`;
+perheadtailtail=`expr ${coin[headtailtail]} \* 100`;
+perheadtailtail=`expr $perheadtailtail / 25`;
+echo "percentage of headheadhead is $perheadheadhead"
+echo "percentage of tailtailtail is $pertailtailtail"
+echo "percentage of headheadtail is $perheadheadtail"
+echo "percentage of headtailtail is $perheadtailtail"
