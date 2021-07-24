5540f2444f597c222413496fa25c8accdcecd4c3
rojajullapalli <rojajules@gmail.com>
2021-07-24 18:46:04 +0530

    loop to create doublet combination hh,ht,tt,th

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -2,28 +2,40 @@
 echo "flip Coin Combination"
 declare  -A  coin;
 
-coin[heads]=0;
-coin[tails]=0;
+coin[headhead]=0;
+coin[tailtail]=0;
+coin[headtail]=0;
 count=0;
 
-addtoDictionary(){
-if [ $1 -eq 0 ]
-then
-coin[heads]=`expr ${coin[heads]} + 1`;
-else
-coin[tails]=`expr ${coin[tails]} + 1`;
-fi
+addtoDictionary()
+{
+  if [ $1 -eq 0 ]
+  then
+      coin[headhead]=`expr ${coin[headhead]} + 1`;
+  elif [ $1 -eq 1 ]
+  then
+      coin[tailtail]=`expr ${coin[tailtail]} + 1`;
+  else
+     coin[headtail]=`expr ${coin[headtail]} + 1`;
+  fi
 }
 
 while [ $count -le 25 ]
 do
-flip=$((RANDOM%2));
-addtoDictionary $flip;
-((++count))
+  flip=$((RANDOM%3)); 
+  addtoDictionary $flip;
+  ((++count))
 done
 
-perhead=`expr ${coin[heads]} \* 100`;
-perhead=`expr $perhead / 25`;
-pertail=`expr 100 - $perhead`;
-echo "percentage of heads is $perhead"
-echo "percentage of tails is $pertail"
+perheadhead=`expr ${coin[headhead]} \* 100`;
+perheadhead=`expr $perheadhead / 25`;
+pertailtail=`expr ${coin[tailtail]} \* 100`;
+pertailtail=`expr $pertailtail / 25`;
+perheadtail=`expr ${coin[headtail]} \* 100`;
+perheadtail=`expr $perheadtail / 25`;
+echo "Total headhead count ${coin[headhead]}"
+echo "Total tailtail count ${coin[tailtail]}"
+echo "Total headtail count ${coin[headtail]}"
+echo "percentage of headhead is $perheadhead"
+echo "percentage of tailtail is $pertailtail"
+echo "percentage of headtail is $perheadtail"
